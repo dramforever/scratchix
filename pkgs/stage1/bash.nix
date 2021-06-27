4346f342e2f9996d1050ad2dc57cda9440ebe162
Gytis Ivaskevicius <me@gytis.io>
2021-06-27 11:02:37 +0300

    Add bash

---
+++ pkgs/stage1/bash.nix
@@ -0,0 +1,19 @@
+{ mkDerivation, fetchurl, bison }:
+
+mkDerivation {
+  name = "bash-5.1";
+
+  src = fetchurl {
+    url = "https://ftp.gnu.org/gnu/bash/bash-5.1.tar.gz";
+    hash = "sha256-zAEryGBAbc9C9kQxvNPS+nVgwCkVpgGrqc1Zejkym6o=";
+  };
+
+  NIX_BUILD_CORES = 1;
+
+  buildInputs = [ bison ];
+
+  configureFlags = [
+  ];
+
+}
+
